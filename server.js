e14a51425ef067e72fb9c0c42cee1dc443fc656c
Quân (Máy cty) <pthyst2@gmail.com>
2022-07-28 10:25:17 +0700

    Build 2

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/client'));
+app.use(express.static(__dirname + '/docs'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
+  res.sendFile(path.join(__dirname + '/docs/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
